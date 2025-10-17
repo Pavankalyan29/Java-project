f96371320c1a317f15e44e3934de78df206118f6
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-10-17 15:57:37 +0530

    changed java code file name

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class AASCIISum{
+public class test{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String s = "Helloworld";
