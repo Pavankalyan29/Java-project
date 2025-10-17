c0fcf5dd60a601a6176a7e66180b4ec6e19e27c6
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-10-17 22:11:22 +0530

    Jenkins file has been Updated

--- AASCIISum.java
+++ AASCIISum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class test{
+public class AASCIISum{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String s = "Helloworld";
